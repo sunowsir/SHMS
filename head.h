84f302ed40e851aefeef64976e2ef529af40f6ed
sunowsir <sunowsir@protonmail.com>
2018-09-26 20:17:49 +0800

    update all

---
+++ head.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name:    head.h
+	> Author:       sunowsir
+	> Mail:         sunowsir@protonmail.com
+	> Created Time: 2018年09月24日 星期一 09时20分14秒
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <wait.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#endif
+
+//Wang Xinyang
