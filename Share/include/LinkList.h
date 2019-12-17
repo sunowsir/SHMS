0120649961b95895ae913991b4993ed0cbdee7b5
sunowsir <sunow.wang@gmail.com>
2019-12-17 17:30:05 +0800

    update : dir structure

---
+++ Share/include/LinkList.h
@@ -0,0 +1,39 @@
+/*
+* File Name: LinkList.h
+* Author:    sunowsir
+* Mail:      sunowsir@protonmail.com
+* GitHub:    github.com/sunowsir
+* Created Time: 2018年11月16日 星期五 10时11分15秒
+*/
+
+#ifndef _LINKLIST_H
+#define _LINKLIST_H
+
+#include "needHead.h"
+
+typedef struct LinkNode {
+    char *IP;
+    int sockFd;
+    struct LinkNode *next;
+} LinkNode;
+
+typedef struct LinkList {
+    LinkNode head;
+    int length;
+} LinkList;
+
+LinkList *linkInit();
+
+LinkNode *linkGetNewNode(char *, int /* Port */);
+
+void linkInsert(LinkList *, char *, int /* Port */);
+
+void linkErase(LinkList *, LinkNode *);
+
+/* return : success(0), false(1) */
+
+int linkSearch(LinkList *, /* const char * */ int);
+
+void linkClear(LinkList *);
+
+#endif
